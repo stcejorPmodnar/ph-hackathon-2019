87ea5083c00f8b7d0741b262358d61574c4235a2
lol-cubes <arinmkhare@gmail.com>
2019-08-28 18:33:07 -0400

    begin find in file and argument handling

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,66 @@
 #include <string>
 #include <iostream>
 #include <sstream>
+#include <cstring>
 
 using namespace std;
+
+bool replace(std::string& str, const std::string& from, const std::string& to) {
+    size_t start_pos = str.find(from);
+    if(start_pos == std::string::npos)
+        return false;
+    str.replace(start_pos, from.length(), to);
+    return true;
+}
+
+bool file_exists(const char *fileName) {
+    ifstream file(fileName);
+    return file.good();
+}
 
 int main(int argc, char **argv) {
     /*
     Reads a text file and displays contents to the screen 
     */
+
+   string encoding;
+
+   // Argument handling: 
+    for ( int i = 1; i < argc; i++ ) {
+        
+        string curArg = argv[i];
+        
+        switch (curArg) {
+            case "-h":
+                // for ( int x = i + 1; x < i + 6; x++) {
+                //     switch (x) {
+                //         case i + 1:
+                //     }
+                // }
+                //something should happen here
+            break;
+            case "-e":
+                if ( argv[i++] !== "-l" ){
+                    encoding = argv[i++];
+                }
+            break;
+        }
+    }
+
+//    if ( argc > 2 ) {
+//         cout << "Sorry, only one file can be opened with the free version." << endl;
+//         return 1;
+//    }
+//    else if (argc == 1) {
+//         cout << "Specify an input file" << endl;
+//         return 1;
+//    }
+//    else {
+//        if (!file_exists(argv[1])) {
+//             cout << argv[1] << " is not a file." << endl;
+//             return 1;
+//         }
+//    }
 
     string line;
     ifstream file (argv[1]);
@@ -27,26 +80,35 @@ int main(int argc, char **argv) {
         }
     }
 
-    // convert file_contents to char array
-    char * file_contents_char_arr = new char[file_contents.length() + 1];
-    strcpy(file_contents_char_arr, file_contents.c_str());
-
     // setup screen and input
     initscr();
     cbreak();
     noecho();
     keypad(stdscr, TRUE);
-    scrollok(stdscr, TRUE);
 
     // how far away each coord of the cursor is from its original position
     int add_y;
     int add_x;
 
+    // the text to be displayed to the screen
+    string screen_text = file_contents;
+
     // mainloop
     while (true) {
         int ch = getch();
         clear();
-        printw(file_contents_char_arr);
+
+        string cropped_screen_text;
+        istringstream screen_text_iss(screen_text);
+        int i;
+        string line;
+        for ( int i = 0; i < COLS && getline(screen_text_iss, line); i++ ) {
+            cropped_screen_text.append(line + '\n');
+        }
+
+        char * screen_text_char = new char[cropped_screen_text.length() + 1];
+        strcpy(screen_text_char, cropped_screen_text.c_str());
+        printw(screen_text_char);
         refresh();
         if (ch == 113) /* "q" */{
             endwin();
@@ -59,11 +121,30 @@ int main(int argc, char **argv) {
             add_x -= 1;
         } else if (ch == 260) /* left arrow but scroll right */ {
             add_x += 1;
+        } else if (ch == 20) /* ^t (find in file) */ {
+            // change bottom line of screen text to blank line
+            
+            string last_line;
+            istringstream screen_text_iss(screen_text);
+            for ( string line; getline(screen_text_iss, line); ) {
+                last_line = line;
+            }
+
+            string new_last_line;
+            for ( int i = 0; i < COLS; i++ ) {
+                new_last_line.append(" ");
+            }
+
+            replace(screen_text, last_line, new_last_line);
         }
+        // wmove cursor only if x and y will be inside of window bounds;
+       
         int x;
         int y;
         getyx(stdscr, x, y);
-        wmove(stdscr, y + add_y, x + add_x);
+        if ((0 <= x + add_x <= COLS) && (0 <= y + add_y <= LINES)) {
+            wmove(stdscr, y + add_y, x + add_x);
+        }
     }
     return 0;
 }
