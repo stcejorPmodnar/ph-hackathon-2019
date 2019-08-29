25aa82e2925b767ea7fbf0e65a0813e690540569
lol-cubes <arinmkhare@gmail.com>
2019-08-29 00:37:49 -0400

    added exit keyboard shortcut

--- file-reader.py
+++ file-reader.py
@@ -1,7 +1,8 @@
 import curses
+import curses.textpad
 from os.path import abspath
 import sys
-from time import sleep
+import time
 
 
 class FileText:
@@ -22,7 +23,40 @@ class FileText:
         return '\n'.join([''.join(line) for line in transposed])
 
 
+class Line:
+    def __init__(self, length):
+        self.chars = [' ' for _ in range(length)]
+    
+    def replace(self, index, char):
+        self.chars[index] = char
+    
+    @property
+    def display(self):
+        return ''.join(self.chars)
+
+
 def main(stdscr, file):
+    
+    def ask_to_quit(iteration, last_one):
+
+        while True:   
+            key = stdscr.getch()
+            if key == 121: # y
+                if last_one:
+                    sys.exit()
+                else:
+                    return
+            elif key == 110: # n
+                pass
+
+            stdscr.clear()
+            base_string = 'Are you sure you want to quit? [y/n]'
+            added_sures = ''.join(['you\'re sure ' for _ in range(iteration)])
+            final_string = base_string[:13] + added_sures + base_string[13:]
+            stdscr.addstr(final_string)
+            stdscr.refresh()
+
+            time.sleep(0.01)
 
     # read file
     with open(abspath(file), 'r') as f:
@@ -38,38 +72,84 @@ def main(stdscr, file):
             file_text.replace(x, y, char)
 
     lines_start = 0
-    lines_stop = curses.LINES - 1
+    lines_stop = curses.LINES
     cols_start = 0
     cols_stop = curses.COLS - 1
 
-    stdscr.nodelay(True)
-    key = ""
+    add_x = 0
+    add_y = 0
+
+    stdscr.nodelay(1)
     while True:
-        try:
-            key = stdscr.getkey()
-            if str(key) == "q":
-                sys.exit()
-            elif str(key) == 'KEY_DOWN':
-                y, x = stdscr.getyx()
-                cursor.move(y + 1, x)
-            elif str(key) == 'KEY_UP':
-                y, x = stdscr.getyx()
-                cursor.move(y - 1, x)
-            elif str(key) == 'KEY_LEFT':
-                y, x = stdscr.getyx()
-                cursor.move(y, x + 1)
-            elif str(key) == 'KEY_RIGHT':
-                y, x = stdscr.getyx()
-                cursor.move(y, x - 1)
-        except Exception:
-            # no input
-            pass
+        
+        x_changed = False
+        y_changed = False
+
+        key = stdscr.getch()
+        
+        if key != -1:
+            with open('output', 'w') as f:
+                f.write(str(key))
+
+        if key == 5: # ^e (quit)
+            for i in range(10):
+                ask_to_quit(i, False)
+            ask_to_quit(10, True)
+        
+        elif key == 117: # u
+            add_y -= 1
+            y_changed = True
+        elif key == 100: # d
+            add_y += 1
+            y_changed = True
+        elif key == 114: # r
+            add_x += 1
+            x_changed = True
+        elif key == 108: # l
+            add_x -= 1
+            x_changed = True
+
         stdscr.clear()
         stdscr.addstr(file_text.display(lines_start, lines_stop, cols_start, cols_stop))
-        stdscr.refresh()
-        
-        sleep(0.01)
 
+        # move cursor
+        y, x = stdscr.getyx()
+        new_x = x + add_x
+        new_y = y + add_y
+        if x_changed:
+            if new_x >= curses.COLS:
+                # scroll if possible
+                if len(file_text.grid) >= new_x:
+                    cols_start += 1
+                    cols_stop += 1
+                add_x -= 1
+                new_x -= 1
+            elif new_x < 0:
+                if cols_start > 0:
+                    cols_start -= 1
+                    cols_stop -= 1
+                add_x += 1
+                new_x += 1
+        elif y_changed:
+            if new_y > curses.LINES:
+                # scroll if possible
+                if len(file_text.grid[0]) >= new_y:
+                    lines_start += 1
+                    lines_stop += 1
+                add_y -= 1
+                new_y -= 1
+            elif new_y < 0:
+                if lines_start > 0:
+                    lines_start -= 1
+                    lines_stop -= 1
+                add_y += 1
+                new_y += 1
+        
+        stdscr.move(new_y, new_x)
+
+        stdscr.refresh()
+
+        time.sleep(0.01)
 
 if __name__ == "__main__":
 
